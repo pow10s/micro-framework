475a5902c18da140573120d91a6266e0f849a897
stosdima <stosdima@gmail.com>
2015-03-19 23:44:11 +0200

    Input options

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 if (isset($_GET['action'])){
- 
+    if($_GET['action'] == 'view_message'){
+        echo 'view';
+    }
+    elseif($_GET['action'] == 'create_message'){
+        echo 'create';
+    }
+    elseif($_GET['action'] == 'update_message'){
+        echo 'update';
+    }
     }
 
